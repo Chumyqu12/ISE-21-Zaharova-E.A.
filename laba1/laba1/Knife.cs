c2887a8899040eb360f0b79e16da4784200cabf4
Hom <Hom@1234>
2017-11-10 19:16:56 +0400

    ghscjkdklxckcl

---
+++ laba1/laba1/Knife.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    class Knife
+    {
+        public void Clean_potato(Potato p)
+        {
+            if (p.Have_scin)
+            {
+                p.Have_scin = false;
+            }
+        }
+        public void Clean_carrots(Carrot p)
+        {
+            if (p.Have_scin)
+            {
+                p.Have_scin = false;
+            }
+        }
+        public void Clean_onion(Onion p)
+        {
+            if (p.Have_scin)
+            {
+                p.Have_scin = false;
+            }
+        }
+    
+
+        public void Cutting_c(Chicken t)
+        {
+            if (t.cutting)
+            {
+                t.cutting = false;
+            }
+
+        }
+
+
+    }
+}
